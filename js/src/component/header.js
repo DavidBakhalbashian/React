013de278f44b796e045a813620ae1b1434842e1c
David <123312897+DavidBakhalbashian@users.noreply.github.com>
2023-05-18 20:07:17 +0400

    Add files via upload

---
+++ js/src/component/header.js
@@ -0,0 +1,24 @@
+
+
+function Header(props) {
+    return(
+  <div id = "header">
+    <div id = "logo">
+    <img src = "logo.png"></img>
+    
+    </div>
+    <div id = "select">
+        <ul>
+            <li>Как пользоваться?</li>
+            <li>Мои записи</li>
+            <li>Записаться к врачу</li>
+            <li> <img src = "logo1.png"></img></li>
+        </ul>
+    </div>
+   
+    
+  </div>
+    )
+}
+
+export default Header
